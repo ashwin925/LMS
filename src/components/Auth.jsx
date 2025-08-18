16c0959e9ab9de1fffa0b9e4473d9260c379bac1
ashwin925 <ashwinsundar90250@gmail.com>
2025-08-18 15:45:25 +0530

    upt

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -31,7 +31,7 @@ const handleGoogleLogin = async () => {
         className="px-6 py-3 bg-white border border-gray-300 rounded-lg shadow-sm flex items-center"
       >
         <img 
-          src="https://www.google.com/favicon.ico" 
+          src="https://www.google.com/favico" 
           alt="Google logo" 
           className="w-5 h-5 mr-2"
         />
